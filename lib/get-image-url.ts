77fe15abf3d75210c6c1aea467728f5444456a32
Ankit Kumar <ankitkumar8396836615@gmail.com>
2026-03-26 16:08:45 +0530

    Update: CDN implimented

--- lib/get-image-url.ts
+++ lib/get-image-url.ts
@@ -1,5 +1,6 @@
 
-const STORAGE_BASE_URL = process.env.EXPO_PUBLIC_STORAGE_BASE_URL || "https://api.pentasent.com/storage/v1/object/public";
+const STORAGE_BASE_URL = process.env.EXPO_PUBLIC_STORAGE_BASE_URL || "https://cdn.pentasent.com/storage/object/public";
+// const STORAGE_BASE_URL = process.env.EXPO_PUBLIC_STORAGE_BASE_URL || "https://api.pentasent.com/storage/v1/object/public";
 const BUCKET = process.env.EXPO_PUBLIC_BUCKET || "avatars";
 
 /**
